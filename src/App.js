96b7a4fcdfd15b7ea57e3c4eafadfe1160ce4e97
DoomsMirtz <tsuryavardhan21@gmail.com>
2021-08-17 12:27:28 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home'
+import Services from './components/pages/Services'
+import Products from './components/pages/Products'
+import SignUp from './components/pages/SignUp'
+import Darkmode from 'darkmode-js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+      <Route path='/' exact component = {Home} />
+      <Route path='/services' component = {Services} />
+      <Route path='/products' component = {Products} />
+      <Route path='/sign-up' component = {SignUp} />
+      </Switch>
+      </Router>
+    </>
+      
+      );
+    }
+    // new Darkmode().showWidget();
+    
+    const options = {
+      bottom: '64px', // default: '32px'
+      right: 'unset', // default: '32px'
+      left: '32px', // default: 'unset'
+      time: '0.5s', // default: '0.3s'
+      mixColor: '#fff', // default: '#fff'
+      backgroundColor: '#fff',  // default: '#fff'
+      buttonColorDark: '#100f2c',  // default: '#100f2c'
+      buttonColorLight: '#fff', // default: '#fff'
+      saveInCookies: false, // default: true,
+      label: '🌓', // default: ''
+      autoMatchOsTheme: true // default: true
+    }
+    
+    const darkmode = new Darkmode(options);
+    darkmode.showWidget();
 
 export default App;
